9dea646321cedd3b69f0aa927bdbdbc983fcd218
Samoilenko Yuri <kinnalru@yandex.ru>
2023-09-28 17:11:56 +0300

    Try to Stop DelayedJob after whole work_off (`loop`)
    instread trying after each job execution (`perform`)

---
+++ spec/delayed_job_plugin/oom_limiter_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe WorkerKiller::DelayedJobPlugin::OOMLimiter do
+
+  let(:killer) {double}
+  subject(:plugin){ described_class.new(killer: killer) }
+
+  context 'DelayedJob initialization' do
+    let(:lifecycle) { double }
+    subject(:instance) { plugin.new(lifecycle) }
+    
+    it  do
+      expect(lifecycle).to receive(:after).with(:perform).and_yield
+      expect(lifecycle).to receive(:after).with(:loop).and_yield
+      instance
+    end
+  end
+end
+
